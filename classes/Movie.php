48aefddb96600a89261775bd617dbf549ad0fedc
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-02 16:17:33 +0200

    Stampo a schermo un primo oggetto

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,9 +1,26 @@
 <?php
 
 class Movie{
+
     private $title;
     private $genre;
     private $publishDate;
 
+    function __construct($_title, $_genre, $_publishDate){
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->publishDate = $_publishDate;
+    }
 
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function getGenre(){
+        return $this->genre;
+    }
+
+    public function getPublishDate(){
+        return $this->publishDate;
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+    require_once __DIR__ . '/classes/Movie.php';
+
+    $movies = [];
+    $movies[] = new Movie("Io sono leggenda", "azione", 2007);
+
+    // var_dump($movies);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,13 @@
     <title>PHP OOP 1</title>
 </head>
 <body>
-    
+    <h1>Movies</h1>
+    <ol>
+        <?php foreach ($movies as $movie) { ?>
+            <li>
+                <?php echo $movie->getTitle() . ' - ' . $movie->getGenre() . ' - ' . $movie->getPublishDate(); ?>
+            </li>
+        <?php } ?>
+    </ol>
 </body>
 </html>
